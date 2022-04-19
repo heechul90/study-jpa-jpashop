6345b7ef519924c3ae1c58f2b2232873da99f7e0
hclee <heechul4296@gmail.com>
2022-04-19 19:44:41 +0900

    상품 등록

--- src/test/java/jpabook/jpashop/service/ItemServiceTest.java
+++ src/test/java/jpabook/jpashop/service/ItemServiceTest.java
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -24,6 +25,7 @@ class ItemServiceTest {
     private ItemRepository itemRepository;
 
     @Test
+    @Rollback(value = false)
     void saveItem() {
         //given
         Item item = new Item();
